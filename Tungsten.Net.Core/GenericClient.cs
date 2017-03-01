abd23ce614e27e302ec5bf97bb7167bf3972984e
Jordan <apps@duerksen.us>
2017-03-01 15:28:05 -0600

    Moved W.Net.GenericServer into it's own file.

--- Tungsten.Net.Core/GenericClient.cs
+++ Tungsten.Net.Core/GenericClient.cs
@@ -55,13 +55,5 @@ namespace W.Net
             };
         }
     }
-
-    /// <summary>
-    /// Extends SecureServer by supporting generics
-    /// </summary>
-    /// <typeparam name="TType">The type to transmit and receive</typeparam>
-    public class GenericServer<TType> : W.Net.Sockets.SecureServer<GenericClient<TType>>
-    {
-    }
 }
 
