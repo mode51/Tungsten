3c51ef637bc9eb24c1016668d723277381f7c570
Jordan <apps@duerksen.us>
2018-03-31 16:37:04 -0500

    Jordanized the code a bit

--- Src/Tungsten.Encryption/MD5.cs
+++ Src/Tungsten.Encryption/MD5.cs
@@ -33,17 +33,17 @@ namespace W.Encryption
 
             // Create a new Stringbuilder to collect the bytes
             // and create a string.
-            StringBuilder sBuilder = new StringBuilder();
+            var result = new StringBuilder();
 
             // Loop through each byte of the hashed data 
             // and format each one as a hexadecimal string.
             for (int i = 0; i < data.Length; i++)
             {
-                sBuilder.Append(data[i].ToString("x2"));
+                result.Append(data[i].ToString("x2"));
             }
 
             // Return the hexadecimal string.
-            return sBuilder.ToString();
+            return result.ToString();
         }
 
         /// <summary>
@@ -60,7 +60,7 @@ namespace W.Encryption
                 string hashOfInput = GetMd5Hash(input, md5);
 
                 // Create a StringComparer an compare the hashes.
-                StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+                var comparer = StringComparer.OrdinalIgnoreCase;
 
                 if (0 == comparer.Compare(hashOfInput, hash))
                 {
